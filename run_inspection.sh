a55dcfa9f13f8edf4a6e3db57b875462b05e3b70
Longyi Qi <longyi@chaione.com>
2011-11-02 11:08:48 -0500

    use the UGLY way to invoke oclint inspection

--- run_inspection.sh
+++ run_inspection.sh
@@ -1,24 +1,23 @@
 #! /bin/bash
 
-INCLUDES=''
-INCLUDES="$INCLUDES -x objective-c -arch x86_64"
-INCLUDES="$INCLUDES -isysroot /Developer/SDKs/MacOSX10.7.sdk"
-INCLUDES="$INCLUDES -F framework"
-INCLUDES="$INCLUDES -F framework/CocoaLibSpotify.framework/Versions/A/Frameworks"
-INCLUDES="$INCLUDES -I /usr/lib/clang/3.0/include"
+LANGUAGE=objective-c
+ARCH=x86_64
+SYSROOT=/Developer/SDKs/MacOSX10.7.sdk
+CLANG_INCLUDE=/usr/lib/clang/3.0/include
+PCH_PATH=repeatify/repeatify-Prefix.pch
 
+INCLUDES=''
 for folder in `find lib -type d`
 do
   INCLUDES="$INCLUDES -I $folder"
 done
+INCLUDES="$INCLUDES -F framework"
+INCLUDES="$INCLUDES -F framework/CocoaLibSpotify.framework/Versions/A/Frameworks"
 
-INCLUDES="$INCLUDES -include repeatify/repeatify-Prefix.pch"
-
-mo_path=$1
-
+FILES=''
 for file in `find repeatify -name "*.m"`
 do
-  #echo "$mo_path $INCLUDES $file | grep '^repeatify'"
-  echo "$mo_path $file"
-  $mo_path $INCLUDES $file | grep '^repeatify'
+  FILES="$FILES $file"
 done
+
+oclint -x $LANGUAGE -arch $ARCH -isysroot=$SYSROOT -I $CLANG_INCLUDE $INCLUDES -include $PCH_PATH $FILES
